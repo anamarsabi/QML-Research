ab834e5f5b0373f171d2d0020bd1751518b378ae
Amarfly <anamarsabi@gmail.com>
2024-05-28 15:40:12 +0200

    jaxjit and its results

--- qsvm_jit_sizetest.py
+++ qsvm_jit_sizetest.py
@@ -5,9 +5,9 @@ import numpy as np
 import pennylane as qml
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import time
 from argparse import ArgumentParser
 import time
+import filelock
 
 from catalyst import qjit, measure, cond, for_loop, while_loop
 from jax import numpy as jnp
@@ -61,7 +61,7 @@ def performance(y_train_pred, y_train, y_test_pred, y_test, size):
 dim = '3x3'
 folder = 'efficiency_study'
 cat_size = 200
-
+csv_file_path='./results/'+dim+'/qsvm_jit_32pca.csv'
 # Number of Principal components to reduce the dataset's dimensionality
 n_pca=32
 
@@ -77,27 +77,6 @@ np.random.seed(42)
 # Reduce the dimensionality of the dataset with Principal Component Analysis
 ppt_ratio='1'
 
-parser = ArgumentParser()
-parser.add_argument('size')
-args = parser.parse_args()
-size=args.size
-
-start= time.time()
-x_train = np.genfromtxt('./dataset/'+folder+'/train/x_n_'+str(size)+'.csv', delimiter=",",dtype=None)
-
-
-pca = PCA(n_components = n_pca)
-xs_train = pca.fit_transform(x_train)
-print(xs_train.shape)
-
-x_test = np.genfromtxt('./dataset/'+dim+'/test/x_test.csv', delimiter=",",dtype=None)
-xs_test = pca.transform(x_test)
-
-
-y_train = np.genfromtxt('./dataset/'+folder+'/train/y_n_'+str(size)+'.csv', delimiter=",",dtype=None)
-y_test = np.genfromtxt('./dataset/'+dim+'/test/y_test.csv', delimiter=",",dtype=None)
-
-
 # Amplitude encoding of 64 variables using 5 qubits (can encode up to 32 inputs)
 # Number of qubits of the system
 nqubits = 5
@@ -106,6 +85,7 @@ dev = qml.device("lightning.qubit", wires = nqubits)
 
 # We define de circuit of our kernel. We use AmplitudeEmbedding which returns an
 # operation equivalent to amplitude encoding of the first argument
+@qjit
 @qml.qnode(dev)
 def kernel_circ(a,b):
     qml.AmplitudeEmbedding(a, wires=range(nqubits), pad_with=0, normalize=True)
@@ -114,51 +94,96 @@ def kernel_circ(a,b):
     # computational basis
     return qml.probs(wires=range(nqubits))
 
+#start_jit=time.time()
 # Now we use the qjit() decorator to jit the above quantum function
-jitted_kernel = qjit(kernel_circ)
+#jitted_kernel = qjit(kernel_circ)
+#end_jit=time.time()
+#print('Jit time: ', end_jit-start_jit)
 
 # We use SVM of scikit-learn with a custom kernel. 
 # This function computes a matrix of kernel evaluations of the data samples,
 # known as Gram matrix
 def qkernel(A, B):
-  return jnp.array([[jitted_kernel(a,b)[0] for b in B] for a in A])
+  return jnp.array([[kernel_circ(a,b)[0] for b in B] for a in A])
 
-# Metrics scores we will save in a pandas dataframe
-acc_scores=[]
-f1_scores=[]
-prec_scores=[]
-rec_scores=[]
-bacc_scores=[]
-sep_scores=[]
-ppt_scores=[]
-nppt_scores=[]
 
-# Training of the SVM, compute predictions and metrics 
-svm = SVC(kernel = qkernel).fit(xs_train, y_train)
-y_train_pred=svm.predict(xs_train)
-y_test_pred=svm.predict(xs_test)
+for size in range(100,201,100):
+   start=time.time()
+   x_train = np.genfromtxt('./dataset/'+folder+'/train/x_n_'+str(size)+'.csv', delimiter=",",dtype=None)
 
-tr_acc = accuracy_score(y_train_pred, y_train)
-acc_scores.append(accuracy_score(y_test_pred, y_test))
-f1_scores.append(f1_score(y_test, y_test_pred))
-prec_scores.append(precision_score(y_test, y_test_pred))
-rec_scores.append(recall_score(y_test, y_test_pred))
-bacc_scores.append(balanced_accuracy_score(y_test, y_test_pred))
-acc_per_type=detailed_accuracy(y_test_pred, cat_size)
-sep_scores.append(acc_per_type['SEP_acc'])
-ppt_scores.append(acc_per_type['PPT_acc'])
-nppt_scores.append(acc_per_type['NPPT_acc'])
 
-performance(y_train_pred, y_train, y_test_pred, y_test,cat_size)
-# Create a dictionary to sumarise metric's scores
-d={'PPT_ratio': ppt_ratio, 'acc_train': tr_acc, 'accuracy': acc_scores, 'f1': f1_scores,
+   pca = PCA(n_components = n_pca)
+   xs_train = pca.fit_transform(x_train)
+   
+
+   x_test = np.genfromtxt('./dataset/'+dim+'/test/x_test.csv', delimiter=",",dtype=None)
+   xs_test = pca.transform(x_test)
+
+
+   y_train = np.genfromtxt('./dataset/'+folder+'/train/y_n_'+str(size)+'.csv', delimiter=",",dtype=None)
+   y_test = np.genfromtxt('./dataset/'+dim+'/test/y_test.csv', delimiter=",",dtype=None)
+
+   # Metrics scores we will save in a pandas dataframe
+   acc_scores=[]
+   f1_scores=[]
+   prec_scores=[]
+   rec_scores=[]
+   bacc_scores=[]
+   sep_scores=[]
+   ppt_scores=[]
+   nppt_scores=[]
+
+   # Training of the SVM, compute predictions and metrics 
+   svm = SVC(kernel = qkernel).fit(xs_train, y_train)
+   y_train_pred=svm.predict(xs_train)
+   y_test_pred=svm.predict(xs_test)
+   end=time.time()
+
+   tr_acc = accuracy_score(y_train_pred, y_train)
+   acc_scores.append(accuracy_score(y_test_pred, y_test))
+   f1_scores.append(f1_score(y_test, y_test_pred))
+   prec_scores.append(precision_score(y_test, y_test_pred))
+   rec_scores.append(recall_score(y_test, y_test_pred))
+   bacc_scores.append(balanced_accuracy_score(y_test, y_test_pred))
+   acc_per_type=detailed_accuracy(y_test_pred, cat_size)
+   sep_scores.append(acc_per_type['SEP_acc'])
+   ppt_scores.append(acc_per_type['PPT_acc'])
+   nppt_scores.append(acc_per_type['NPPT_acc'])
+
+   print('Training and prediction time: ', end-start)
+
+   performance(y_train_pred, y_train, y_test_pred, y_test,cat_size)
+   # Create a dictionary to sumarise metric's scores
+   d={'PPT_ratio': ppt_ratio, 'acc_train': tr_acc, 'accuracy': acc_scores, 'f1': f1_scores,
    'precision': prec_scores, 'recall': rec_scores, 'balanced_accuracy': bacc_scores,
-   'SEP_accuracy': sep_scores, 'PPT_accuracy': ppt_scores, 'NPPT_accuracy': nppt_scores}
+   'SEP_accuracy': sep_scores, 'PPT_accuracy': ppt_scores, 'NPPT_accuracy': nppt_scores,
+   'time': end-start  }
+
+
+   lock = filelock.FileLock(csv_file_path+'.lock')
+   print('Acquiring lock ...')
+   with lock:
+      print('Lock acquired')
+      # Create a dictionary to sumarise metric's scores
+      d={'size': size, 'PPT_ratio': ppt_ratio, 'acc_train': tr_acc, 'accuracy': acc_scores, 'f1': f1_scores,
+    'precision': prec_scores, 'recall': rec_scores, 'balanced_accuracy': bacc_scores,
+    'SEP_accuracy': sep_scores, 'PPT_accuracy': ppt_scores, 'NPPT_accuracy': nppt_scores,
+    'time': end-start}
+  
+      # Create a DataFrame with the provided data
+      df = pd.DataFrame(data=d)
+      with open(csv_file_path, 'a') as csvfile:
+         # Append the DataFrame to the CSV file
+         df.to_csv(csvfile, header=False, index=False)
+
+
+
+
 
 # Create a pandas dataframe from the dictionary
-df = pd.DataFrame(data=d)
+#df = pd.DataFrame(data=d)
 
 # Save the dataframe to a CSV file
-df.to_csv('./results/'+dim+'/qsvm_test.csv', index=False)
+#df.to_csv('./results/'+dim+'/qsvm_jit_trial.csv', index=False)
 
 
